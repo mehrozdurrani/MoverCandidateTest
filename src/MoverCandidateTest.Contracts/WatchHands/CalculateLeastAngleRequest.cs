2ab96ff0607b59bd774d99349996097df4d41d77
mehrozdurrani <mehrozdurrani@gmail.com>
2023-12-08 19:15:41 +0100

    Improved exceptional handling

--- src/MoverCandidateTest.Contracts/WatchHands/CalculateLeastAngleRequest.cs
+++ src/MoverCandidateTest.Contracts/WatchHands/CalculateLeastAngleRequest.cs
@@ -1,6 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MoverCandidateTest.Contracts.WatchHands
 {
-    public record CalculateLeastAngleRequest(
-        DateTime DateTime
-    );
+    public record CalculateLeastAngleRequest
+    {
+        [Required(ErrorMessage = "DateTime is required")]
+        [DataType(DataType.DateTime, ErrorMessage = "DateTime must be in a valid date and time format")]
+        public DateTime DateTime { get; init; }
+    }
 }
